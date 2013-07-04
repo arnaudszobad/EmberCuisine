4a8d59e5c4b51a17e989823ee433dc9ee5050c8c
Arnaud Szobad <arnaud@kiwixi.com>
2013-07-04 17:56:20 +0200

    better

--- js/app.js
+++ js/app.js
@@ -4,9 +4,10 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
+  this.route('create');
   this.resource('user',function(){
+    this.route('edit',{path:'/edit/:user_id'});
     this.route('recipes');
-    this.route('create');
     this.route('detail',{path:'/detail/:recipe_id'});
   });
 });
@@ -28,11 +29,18 @@ App.Store = DS.Store.extend({
 });
 
 // ####### ROUTES #######
-// App.UserRoute = ember.Route.extend({
-//     renderTemplate:function(){
-//         this.render("user/recipes");
-//     }
-// })
+App.UserRoute = Ember.Route.extend({
+    model:function(){
+        return App.User.find();
+    }
+});
+
+App.UserEditRoute = Ember.Route.extend({
+    model:function(item){
+        console.log(item);
+         return App.User.find(item.id);
+    }
+});
 
 App.UserRecipesRoute = Ember.Route.extend({
     model:function(){
@@ -47,34 +55,73 @@ App.UserRecipesRoute = Ember.Route.extend({
     }
 });
 
-App.UserCreateRoute = Ember.Route.extend({
-    model:function(){
-        return Ember.Object.extend();
-    },
-    events:{
-        save:function(){
-            var title = this.get('controller.title');
-            var isactive = this.get('controller.active');
-            if(isactive == undefined) isactive = false;
-            var newRecipe = App.Recipe.createRecord({title:title,active:isactive});
-            newRecipe.save();
-        },
-        addingredient:function(){
-
-        }
-    }
-});
-
 App.UserDetailRoute = Ember.Route.extend({
     model : function(params){
         return App.Recipe.find(params.id);
     }
 });
 
+App.CreateRoute = Ember.Route.extend({
+    setupController:function(controller,model){
+        // affectation du model au controller pour accès facilité
+        controller.set('model',App.Recipe.createRecord());
+    },
+    deactivate:function(){
+        // rollback quand on quitte la route, si le model est dirty
+        if(this.get('controller.model') && this.get('controller.model').get("isDirty")){
+            this.get('controller.model').get("transaction").rollback();
+        }
+    },
+    events:{
+        save:function(){
+            // pas de sauvegarde sans titre
+            if(this.get("controller.model.title")){
+                this.get('controller.model').save();
+            }
+        },
+        addingredient:function(){
+            // ajout d'un record au model Recipe --> ajout d'un champ input dans l'interface
+            this.get('controller.model.ingredients').pushObject(App.Ingredients.createRecord());
+        },
+        removeingredient:function(item){
+            // suppression d'un ingrédient
+            this.get('controller.model.ingredients').removeObject(item);
+        }
+    }
+});
+
+
+
 // ####### CONTROLLERS #######
 
 
 // ####### FIXTURES #######
+App.User = DS.Model.extend({
+    nom: DS.attr("string"),
+    prenom: DS.attr("string"),
+    recipes:DS.hasMany("App.Recipe")
+})
+App.User.FIXTURES = [
+{
+    id:1,
+    nom:"Szobad",
+    prenom:"Arnaud",
+    recipes:[1,4]
+},
+{
+    id:2,
+    nom:"Maubourguet",
+    prenom:"Nicolas",
+    recipes:[2,6]
+},
+{
+    id:3,
+    nom:"Adjeroud",
+    prenom:"Lotfi",
+    recipes:[3,5,7]
+}
+];
+
 App.Ingredients = DS.Model.extend({
     ing:   DS.attr('string'),
     recipe:DS.belongsTo('App.Recipe')
@@ -121,26 +168,59 @@ App.Ingredients.FIXTURES = [
 App.Recipe = DS.Model.extend({
     title:   DS.attr('string'),
     active:  DS.attr('boolean'),
-    ingredients: DS.hasMany('App.Ingredients')
+    rating:  DS.attr('integer'),
+    ingredients: DS.hasMany('App.Ingredients'),
+    user: DS.belongsTo("App.User")
 });
 App.Recipe.FIXTURES = [
  {
    id: 1,
    title: 'Chocolat aux pruneaux',
    active:true,
-   ingredients:[1,2]
+   ingredients:[1,2],
+   user:1
  },
  {
    id: 2,
-   title: 'Café frappé',
+   title: 'Chocolat aux pruneaux',
    active:true,
-   ingredients:[4,5]
+   ingredients:[1,2],
+   user:2
  },
  {
    id: 3,
+   title: 'Chocolat aux pruneaux',
+   active:true,
+   ingredients:[1,2],
+   user:3
+ },
+ {
+   id: 4,
+   title: 'Café frappé',
+   active:true,
+   ingredients:[4,5],
+   user:1
+ },
+ {
+   id: 5,
+   title: 'Café frappé',
+   active:true,
+   ingredients:[4,5],
+   user:3
+ },
+ {
+   id: 6,
    title: 'Polenta sucrée',
    active:true,
-   ingredients:[6,3,7,2]
+   ingredients:[6,3,7,2],
+   user:2
+ },
+ {
+   id: 7,
+   title: 'Polenta sucrée',
+   active:true,
+   ingredients:[6,3,7,2],
+   user:3
  }
 ];
 
